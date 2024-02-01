b3210d2610ae4ee305583e04d3398408a0dec8a5
Maxsedzik <maksymilian@maxsedzik.com>
2024-02-01 19:01:37 +0100

    Added function that mixes elements

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import { View, Text, StyleSheet } from "react-native";
 import Header from "./components/Header";
 
 const App = () => {
+	let numbers = Array.from({ length: 9 }, (_, index) => index + 1);
+
+	for (let i = numbers.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+	}
+
 	return (
 		<View style={styles.container}>
 			<Header />
