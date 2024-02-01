a379f921826312b10a10260d9664c2f61e15cd42
Maxsedzik <maksymilian@maxsedzik.com>
2024-02-01 18:51:40 +0100

    Added function that mixes elements

--- App.js
+++ App.js
@@ -2,6 +2,18 @@ import { View, Text, StyleSheet } from "react-native";
 import Header from "./components/Header";
 
 const App = () => {
+	// Tworzenie tablicy z liczbami od 1 do 9
+	let numbers = Array.from({ length: 9 }, (_, index) => index + 1);
+
+	// Mieszanie tablicy
+	for (let i = numbers.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+	}
+
+	// Wyświetlanie pomieszanych liczb
+	console.log(numbers);
+
 	return (
 		<View style={styles.container}>
 			<Header />
