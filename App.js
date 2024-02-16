942ff7fafe6b8d50835b9652327ed3d16012a098
Maxsedzik <maksymilian@maxsedzik.com>
2024-02-16 21:00:40 +0100

    Cleaned up

--- App.js
+++ App.js
@@ -3,13 +3,6 @@ import Header from "./components/Header";
 import CardsField from "./components/CardsField";
 
 const App = () => {
-	let numbers = Array.from({ length: 12 }, (_, index) => index + 1);
-
-	for (let i = numbers.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-	}
-
 	return (
 		<View style={styles.container}>
 			<Header />
