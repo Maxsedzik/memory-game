8294a56a4e87702f369fcf3765e4638dc309327f
Maxsedzik <maksymilian@maxsedzik.com>
2024-02-21 22:48:11 +0100

    Added some logic

--- components/CardsField.js
+++ components/CardsField.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import Card from "./Card";
+import AppContext from "../context/AppContext";
 
 const CardsField = () => {
-	let numbers = Array.from({ length: 12 }, (_, index) => index + 1);
-
-	for (let i = numbers.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-	}
+	const { mixedCards, cardSelect } = useContext(AppContext);
 	return (
 		<View style={styles.cardsfield}>
-			{numbers.map((number) => {
-				return <Card key={number} number={number} />;
+			{mixedCards.map((card) => {
+				return (
+					<Card
+						key={card.id}
+						value={card.value}
+						onPress={cardSelect(card.value, card.id)}
+					/>
+				);
 			})}
 		</View>
 	);
--- context/AppContext.js
+++ context/AppContext.js
@@ -3,21 +3,46 @@ import { createContext, useState } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-	let numbers = Array.from({ length: 12 }, (_, index) => index + 1);
+	const [cards, setCards] = useState([
+		{ id: 1, value: "A" },
+		{ id: 2, value: "A" },
+		{ id: 3, value: "B" },
+		{ id: 4, value: "B" },
+		{ id: 5, value: "C" },
+		{ id: 6, value: "C" },
+		{ id: 7, value: "D" },
+		{ id: 8, value: "D" },
+		{ id: 9, value: "E" },
+		{ id: 10, value: "E" },
+		{ id: 11, value: "F" },
+		{ id: 12, value: "F" },
+	]);
 
-	const mixCards = () => {
-		let mixedNumbers = [...numbers];
-		for (let i = mixedNumbers.length - 1; i > 0; i--) {
+	let cardsSelected = [];
+
+	const mixCards = (x) => {
+		let mixedCards = [...x];
+		for (let i = mixedCards.length - 1; i > 0; i--) {
 			const j = Math.floor(Math.random() * (i + 1));
-			[mixedNumbers[i], mixedNumbers[j]] = [mixedNumbers[j], mixedNumbers[i]];
+			[mixedCards[i], mixedCards[j]] = [mixedCards[j], mixedCards[i]];
 		}
-		return mixedNumbers;
+		return mixedCards;
 	};
 
-	let mixedNumbers = mixCards(numbers);
+	let mixedCards = mixCards(numbers);
+
+	function cardSelect(value, id) {
+		cardsSelected.push({ value, id });
+	}
+
+	if (cardsSelected.length === 2) {
+		if (cardsSelected[0].value === cardsSelected[1].value) {
+			//Delete cards from the stack
+		}
+	}
 
 	return (
-		<AppContext.Provider value={{ mixCards, mixedNumbers }}>
+		<AppContext.Provider value={{ mixCards, mixedCards, cardSelect }}>
 			{children}
 		</AppContext.Provider>
 	);
